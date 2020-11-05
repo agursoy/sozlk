bf7a66fbb9b2787c4fa0f461896016c7e391d782
Abdullah Gürsoy <a.gursoy@msn.com>
2020-11-05 10:49:53 +0300

    fast generating

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -141,7 +141,21 @@ export default {
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: false,
+        minifyJS: false,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+      },
+    },
+  },
 
   // Strapi
   strapi: {
